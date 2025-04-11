4e771d90127f5e4e9b6d89a774a5705fdba66275
Linus Walker <rissk_it@proton.me>
2025-04-11 08:52:04 +0630

    chore: shittts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-use csv::Reader;
+use ndarray::Axis;
+use polars::{io::SerReader, prelude::*};
+use rand::seq::SliceRandom;
 
 fn main() {
-    let mut data = Reader::from_path("data/train.csv").unwrap();
-    println!("{:#?}", data.headers());
+    let mut rng = rand::rng();
+    let csv_data = CsvReadOptions::default()
+        .with_has_header(true)
+        .try_into_reader_with_file_path(Some("data/train.csv".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+    let mut data = csv_data.to_ndarray::<Float64Type>(IndexOrder::C).unwrap();
+    println!("{:#?}", data.shape());
+    let mut rows: Vec<_> = data.axis_iter(Axis(0)).collect();
+    rows.shuffle(&mut rng);
+
+    let data = ndarray::stack(
+        Axis(0),
+        &rows.iter().map(|row| row.view()).collect::<Vec<_>>(),
+    )
+    .unwrap();
+    // let ndarray_data = ndarray::array![array_data];
+    // let shape = ndarray_data.shape();
+    //
+    // let data_dev_slice = ndarray_data.slice(s![0..=1000]);
+    // let data_dev = data_dev_slice.t();
+    // let y_dev = data_dev[0].clone();
 }

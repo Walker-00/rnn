d0ce2a5e3f3d972c769378d7b6c31ce65f5e2319
Linus Walker <rissk_it@proton.me>
2025-04-11 10:28:41 +0630

    chore: train data

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ndarray::{Axis, s};
 use polars::{io::SerReader, prelude::*};
 use rand::seq::SliceRandom;
 
+#[allow(unused)]
 fn main() {
     let mut rng = rand::rng();
     let csv_data = CsvReadOptions::default()
@@ -29,7 +30,12 @@ fn main() {
 
     println!("{m}, {n}");
 
-    let data_train = data.slice(s![1000..m, ..]).t();
+    let data_train_slice = data.slice(s![1000..m, ..]);
+    let data_train = data_dev_slice.t();
+
+    let y_train = data_train.slice(s![0, ..]);
+    let x_train_slice = data_train.slice(s![1..n, ..]);
+
     // let x_dev = x_dev_slice / 255.;
 
     // let data_dev = data_dev_slice.t();

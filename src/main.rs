5c81a7acea175700f435d62f8d8d5807d15266f9
Linus Walker <rissk_it@proton.me>
2025-04-11 21:03:12 +0630

    refactor: Array2d

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{f64, usize};
+use std::{f64, iter::zip, usize};
 
 use ndarray::{Array2, ArrayBase, Axis, Dim, OwnedRepr, s};
 use polars::{frame::row, io::SerReader, prelude::*};
@@ -8,9 +8,16 @@ use rayon::iter::{
     IntoParallelRefMutIterator, ParallelBridge, ParallelIterator,
 };
 
-type NDArray = ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>;
+type Array2d = ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>;
 
-// type Wnb = [(NDArray, NDArray); 2];
+// type Wnb = [(Array2d, Array2d); 2];
+
+// fn split_and_normalize(data: &Array2<f64>, from: usize, to: usize) -> (ArrayView1<f64>, Array2<f64>) {
+//     let sliced = data.slice(s![from..to, ..]).t();
+//     let y = sliced.slice(s![0, ..]);
+//     let x = sliced.slice(s![1.., ..]).to_owned() / 255.0;
+//     (y, x)
+// }
 
 fn main() {
     let mut rng = rand::rng();
@@ -41,15 +48,17 @@ fn main() {
     let data_train_slice = data.slice(s![1000..m, ..]);
     let data_train = data_train_slice.t();
 
-    let y_train = data_train.slice(s![0, ..]);
+    let y_train = data_train.row(0);
     let x_train_slice = data_train.slice(s![1..n, ..]);
     let x_train = x_train_slice.to_owned() / 255.0;
 
     println!("{y_train}");
     println!("{:?}", x_train.slice(s![.., 0]).dim());
+
+    let (w1, b1, w2, b2) = gradient_descent(x_train, y_train, 100, 0.1).into();
 }
 
-fn init_params() -> [NDArray; 4] {
+fn init_params() -> [Array2d; 4] {
     let mut rng = rand::rng();
 
     let dist = Uniform::new(0., 1.).unwrap();
@@ -63,22 +72,28 @@ fn init_params() -> [NDArray; 4] {
     [w1, b1, w2, b2]
 }
 
-fn relu(z: &NDArray) -> NDArray {
+fn relu(z: &Array2d) -> Array2d {
     let mut z = z.clone();
     z.par_mapv_inplace(|v| v.max(0.0));
     z
 }
 
-fn deriv_relvu(z: &NDArray) -> NDArray {
+fn deriv_relvu(z: &Array2d) -> Array2d {
     z.mapv(|v| if v > 0. { 1. } else { 0. })
 }
 
-fn softmax(z: &NDArray) -> NDArray {
+fn softmax(z: &Array2d) -> Array2d {
     z.exp() / z.exp().sum()
 }
 
-fn forward_prop(w1: NDArray, b1: NDArray, w2: NDArray, b2: NDArray, x: NDArray) -> [NDArray; 4] {
-    let z1 = w1.dot(&x) + b1;
+fn forward_prop(
+    w1: &Array2d,
+    b1: &Array2d,
+    w2: &Array2d,
+    b2: &Array2d,
+    x: &Array2d,
+) -> [Array2d; 4] {
+    let z1 = w1.dot(x) + b1;
     let a1 = relu(&z1);
     let z2 = w2.dot(&a1) + b2;
     let a2 = softmax(&z2);
@@ -86,7 +101,7 @@ fn forward_prop(w1: NDArray, b1: NDArray, w2: NDArray, b2: NDArray, x: NDArray) 
     [z1, a1, z2, a2]
 }
 
-fn one_hot(y: &NDArray) -> NDArray {
+fn one_hot(y: &Array2d) -> Array2d {
     let max = y.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
     let num_samples = y.len();
     let num_classes = *max as usize + 1;
@@ -113,23 +128,23 @@ fn one_hot(y: &NDArray) -> NDArray {
 }
 
 fn back_prop(
-    z1: NDArray,
-    a1: NDArray,
-    // z2: NDArray,
-    a2: NDArray,
-    w2: NDArray,
-    x: NDArray,
-    y: NDArray,
-) -> [NDArray; 4] {
+    z1: &Array2d,
+    a1: &Array2d,
+    // z2: Array2d,
+    a2: &Array2d,
+    w2: &Array2d,
+    x: &Array2d,
+    y: &Array2d,
+) -> [Array2d; 4] {
     let m = y.len();
-    let one_hot_y = one_hot(&y);
+    let one_hot_y = one_hot(y);
 
     let dz2 = a2 - one_hot_y;
     let dw2 = 1. / m as f64 * dz2.dot(&a1.t());
     // let db2 = 1. / m as f64 * dz2.sum_axis(Axis(2));
     let db2 = dz2.sum_axis(Axis(1)).insert_axis(Axis(1)) * (1. / m as f64);
 
-    let dz1 = w2.t().dot(&dz2) * deriv_relvu(&z1);
+    let dz1 = w2.t().dot(&dz2) * deriv_relvu(z1);
     let dw1 = 1. / m as f64 * dz1.dot(&x.t());
     // let db1 = 1. / m as f64 * dz1.sum_axis(Axis(2));
     let db1 = dz1.sum_axis(Axis(1)).insert_axis(Axis(1)) * (1. / m as f64);
@@ -139,21 +154,72 @@ fn back_prop(
 
 #[allow(clippy::too_many_arguments)]
 fn update_params(
-    w1: NDArray,
-    b1: NDArray,
-    w2: NDArray,
-    b2: NDArray,
-    dw1: NDArray,
-    db1: NDArray,
-    dw2: NDArray,
-    db2: NDArray,
-    alpha: NDArray,
-) -> [NDArray; 4] {
-    let w1 = w1 - &alpha * dw1;
-    let b1 = b1 - &alpha * db1;
+    w1: &Array2d,
+    b1: &Array2d,
+    w2: &Array2d,
+    b2: &Array2d,
+    dw1: &Array2d,
+    db1: &Array2d,
+    dw2: &Array2d,
+    db2: &Array2d,
+    alpha: f64,
+) -> [Array2d; 4] {
+    let w1 = w1 - alpha * dw1;
+    let b1 = b1 - alpha * db1;
 
-    let w2 = w2 - &alpha * dw2;
-    let b2 = b2 - &alpha * db2;
+    let w2 = w2 - alpha * dw2;
+    let b2 = b2 - alpha * db2;
+
+    [w1, b1, w2, b2]
+}
+
+fn get_predictions(a2: &Array2d) -> Vec<usize> {
+    a2.columns()
+        .into_iter()
+        .map(|v| {
+            v.iter()
+                .enumerate()
+                .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+                .map(|(i, _)| i)
+                .unwrap()
+        })
+        .collect()
+}
+
+fn get_accuracy(predictions: Vec<usize>, y: &Array2d) -> f64 {
+    println!("{predictions:?} {y}");
+
+    // let label_classes: Vec<usize> = y
+    //     .axis_iter(Axis(0))
+    //     .map(|row| {
+    //         row.iter()
+    //             .enumerate()
+    //             .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+    //             .map(|(i, _)| i)
+    //             .unwrap()
+    //     })
+    //     .collect();
+
+    let correct = zip(predictions, y)
+        .filter(|(pred, label)| *pred as f64 == **label)
+        .count();
+
+    correct as f64 / y.len_of(Axis(0)) as f64
+}
+
+fn gradient_descent(x: Array2d, y: Array2d, iters: u32, alpha: f64) -> [Array2d; 4] {
+    let (mut w1, mut b1, mut w2, mut b2) = init_params().into();
+
+    for i in 0..iters {
+        let (z1, a1, z2, a2) = forward_prop(&w1, &b1, &w2, &b2, &x).into();
+        let (dw1, db1, dw2, db2) = back_prop(&z1, &a1, &a2, &w2, &x, &y).into();
+        (w1, b1, w2, b2) = update_params(&w1, &b1, &w2, &b2, &dw1, &db1, &dw2, &db2, alpha).into();
+
+        if i % 10 == 0 {
+            println!("Iteration: {i}");
+            println!("Accuracy: {}", get_accuracy(get_predictions(&a2), &y));
+        }
+    }
 
     [w1, b1, w2, b2]
 }

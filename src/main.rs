5565efbdb49e0668208da38da9c9b5b3406e46a7
Linus Walker <rissk_it@proton.me>
2025-04-11 10:24:23 +0630

    chore: .. not ..=

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,16 @@ fn main() {
         &rows.iter().map(|row| row.view()).collect::<Vec<_>>(),
     )
     .unwrap();
-    let data_dev_slice = data.slice(s![0..=1000, ..]);
+    let data_dev_slice = data.slice(s![0..1000, ..]);
     let data_dev = data_dev_slice.t();
 
     let y_dev = data_dev.slice(s![0, ..]);
-    let x_dev = data_dev.slice(s![1..n, ..]);
+    let x_dev_slice = data_dev.slice(s![1..n, ..]);
+
+    println!("{m}, {n}");
+
+    let data_train = data.slice(s![1000..m, ..]).t();
+    // let x_dev = x_dev_slice / 255.;
 
     // let data_dev = data_dev_slice.t();
     // let y_dev = data_dev[0].clone();

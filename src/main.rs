ba0cd94a85f657989a504ee071df01080ef6e7e8
Linus Walker <rissk_it@proton.me>
2025-04-11 09:51:43 +0630

    chore: data dev

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,11 @@ fn main() {
         &rows.iter().map(|row| row.view()).collect::<Vec<_>>(),
     )
     .unwrap();
-    let data_dev_slice = data.slice(s![0..=1000, ..]).t();
+    let data_dev_slice = data.slice(s![0..=1000, ..]);
+    let data_dev = data_dev_slice.t();
+
+    let y_dev = data_dev.first().unwrap();
+
     // let data_dev = data_dev_slice.t();
     // let y_dev = data_dev[0].clone();
 }
